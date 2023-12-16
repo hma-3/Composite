bab75ba2f00ba4269cab46723a56901463ae6ab7
hma-3 <maria.gula0307@gmail.com>
2023-12-17 01:35:59 +0200

    Initial commit

---
+++ Composite.cpp
@@ -0,0 +1,55 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Component {
+public:
+    virtual void display() const = 0;
+};
+
+class Product : public Component {
+    string name;
+public:
+    Product(const string& name) : name(name) {}
+
+    void display() const override {
+        cout << "Product: " << name << endl;
+    }
+
+};
+
+class Box : public Component {
+    Component* components[10];
+    int count = 0;
+public:
+    void add(Component* component) {
+        components[count++] = component;
+    }
+
+    void display() const override {
+        cout << "Box:" << endl;
+        for (int i = 0; i < count; ++i) {
+            components[i]->display();
+        }
+    }
+};
+
+int main() {
+    Product apple("Apple");
+    Product banana("Banana");
+
+    Box fruitBox;
+    fruitBox.add(&apple);
+    fruitBox.add(&banana);
+
+    Product orange("Orange");
+
+    Box biggerBox;
+    biggerBox.add(&fruitBox);
+    biggerBox.add(&orange);
+
+    biggerBox.display();
+
+    return 0;
+}
